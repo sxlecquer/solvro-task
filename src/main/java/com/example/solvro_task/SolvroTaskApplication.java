e875d383ac99a88909e27a0eae427227c05cdbbc
Yan Samokar <ysamokar228@gmail.com>
2024-07-02 17:05:44 +0200

    Create "Solvro task" project

---
+++ src/main/java/com/example/solvro_task/SolvroTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.solvro_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvroTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvroTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/solvro_task/SolvroTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solvro_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvroTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
